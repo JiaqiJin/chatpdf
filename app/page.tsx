aabd53e61033e7093119874ef8f74176729150c0
“Jiaqi_Jin” <fdsam@hotmail.com>
2024-06-03 20:28:55 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,52 @@
  * background color : https://hypercolor.dev/
 */
 import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import { UserButton, useAuth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { link } from "fs";
+import { Link } from "lucide-react";
+import { LogIn } from "lucide-react";
 
 export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+  let firstChat;
   return (
-   <div className="w-screen min-h-screen bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <div className="flex flex-col items-center text-center">
-        <div className="flex items-center">
-          <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
-          <UserButton afterSignOutUrl="/"></UserButton>
-        </div>
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
 
-        <div className="flex mt-2">
-          <Button>Go to Chats</Button>
+          <div className="flex mt-2">
+            {isAuth && firstChat && (
+                  <Button>
+                    Go to Chats
+                  </Button>
+            )}
+          </div>
+
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of students, researchers and professionals to instantly
+            answer questions and understand research with AI
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <p>UPLOAD_FILE</p>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get Started!
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </div>
-   </div>
   );
 }
